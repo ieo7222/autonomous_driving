f2b83a2e75302798875d74f957d638240e1a523e
SEUNGYUP <seungyup7220@gmail.com>
2018-03-30 23:18:23 +0900

    Normalization + Accuravy Test

--- training.py
+++ training.py
@@ -1,17 +1,39 @@
 import tensorflow as tf
 import numpy as np
 
+# def MinMaxScaler(data):
+#     scale=  [ 1.,        200.,         200.,       200.,       200.,
+#     100.,   10000.  ,10000.    ]
+#     print('scale: ',scale)
+#     # noise term prevents the zero division
+#     return data / scale
+
 def MinMaxScaler(data):
-    scale=  [ 1.,        200.,         200.,       200.,       200.,
-    100.,   10000.  ,10000.    ]
-    print('scale: ',scale)
+    numerator = data - np.min(data, 0)
+    denominator = np.max(data, 0) - np.min(data, 0)
+
     # noise term prevents the zero division
-    return data / scale
+    return numerator / (denominator + 1e-7), denominator,np.min(data,0)
+
+
+# def MinMaxScaler(data):
+#     numerator = data - np.min(data, 0)
+#     denominator = np.max(data, 0) - np.min(data, 0)
+#     # noise term prevents the zero division
+#     return numerator / (denominator + 1e-7)
+# 
+# original minMax scaler
+# re back => return * denominator + min = data
 
 with tf.device('/cpu:0'):
 
     xy = np.loadtxt('dataSET1.csv', delimiter=',', dtype=np.float32)
-    xy = MinMaxScaler(xy)
+    xy, denominator, Min = MinMaxScaler(xy)
+    print('denominator')
+    print(denominator)
+    print("min value")
+    print(Min)
+
     x_data = xy[0:4500, 1:-2]
     y_data = xy[0:4500, -2:]
 
@@ -24,8 +46,13 @@ with tf.device('/cpu:0'):
     X = tf.placeholder(tf.float32, shape=[None, 5]) #3:input개수 
     Y = tf.placeholder(tf.float32, shape=[None, 2]) #1:output개수
 
-    W = tf.Variable(tf.random_normal([5,2]), name='weight')
-    b = tf.Variable(tf.random_normal([2]),name='bias')
+    W = tf.Variable(
+[[ 0.13244879 ,-0.35996214],
+ [-0.17254227 ,-0.06763747],
+ [ 0.55679744 , 0.25918636],
+ [-0.24862184 ,-0.09673724],
+ [ 0.03230236 , 0.01645175]], name='weight')
+    b = tf.Variable([0.3112805, 0.3964208],name='bias')
 
     # # Hypothesis
 
@@ -36,20 +63,29 @@ with tf.device('/cpu:0'):
     cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
     # # Minimize
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1e-4)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00)
     train = optimizer.minimize(cost)
+
+    prediction = tf.arg_max(hypothesis, 1)
+    is_correct = tf.equal(prediction, tf.arg_max(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
     # Launch the graph in a session.
     sess = tf.Session()
     # Initializes global variables in the graph.
     sess.run(tf.global_variables_initializer())
     step=0
-    while(True):
+    for i in range(2000):
         step=step+1
-        cost_val,W_val,B_val,H_val,_= sess.run([cost,W,b,hypothesis,train], feed_dict={X: x_data, Y: y_data})
+        cost_val,W_val,B_val,_= sess.run([cost,W,b,train], feed_dict={X: x_data, Y: y_data})
         if step % 2000 == 0:
             step=0            
             print("Cost: ", cost_val)
             print("Weight:")
             print(W_val)
             print("Bias:",B_val)
+    print("Prediction:", sess.run(prediction, feed_dict={X: x_data}))
+    # Calculate the accuracy
+    print("Accuracy: ", sess.run(accuracy, feed_dict={X: x_data, Y: y_data}))
+
+            
